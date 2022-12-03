1b7f8094e74d12a671ce8d6e8a0bb59a04509427
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-12-02 18:00:23 -0600

    Installing firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/noauth/Login';
 import Main from './components/auth/Main';
 
 function App() {
-  const [user, setUser] = useState(undefined);
+  //const [user, setUser] = useState(undefined);
+  // return (
+  //  <div className="App">
+  //   {user ? <Main/> : <Login/>}
+  //   </div>
+  // );
   return (
-    <div className="App">
-    {user ? <Main/> : <Login/>}
-    </div>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/main' element={<Main />} />
+      </Routes>
   );
 }
 
