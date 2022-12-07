d35c38fe92b3fea560c17117c2888d5bb8844608
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-12-07 13:06:39 -0600

    Creating loading page

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -41,4 +41,4 @@ export const AuthContextProvider = ({ children }) => {
   );
 };
 
-export const UserAuth = () => { return useContext(AuthContext); };
+export const UserAuth = () => { return useContext(AuthContext); }
